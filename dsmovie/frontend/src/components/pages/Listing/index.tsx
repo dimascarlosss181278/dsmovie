20f15c534ab88720e05f3039241d82e6a3bb8e23
Dimas Carlos <dimascarlosss@hotmail.com>
2022-05-13 13:56:13 -0300

    MovieCard

--- dsmovie/frontend/src/components/pages/Listing/index.tsx
+++ dsmovie/frontend/src/components/pages/Listing/index.tsx
@@ -1,10 +1,42 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
+  return (
+    <>
+      <Pagination />
+        <div className="container">
+           <div className="row">
 
-    return(
-        <h1> <Pagination /> </h1>
-    )
+             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3 ">
+               <MovieCard />
+             </div>
+
+             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3 ">
+               <MovieCard />
+             </div> 
+
+             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3 ">
+               <MovieCard />
+             </div> 
+
+             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3 ">
+               <MovieCard />
+             </div> 
+
+             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3 ">
+               <MovieCard />
+             </div> 
+
+             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3 ">
+               <MovieCard />
+             </div> 
+
+           </div>
+
+        </div>
+    </>
+  );
 }
 
 export default Listing;
